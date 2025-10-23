817766f11c2d51677afa178a70f0ad9c63ea56ff
Fergus Cooper <fcooper8472@gmail.com>
2025-10-23 17:59:01 +0100

    Error if no submodule checked out

--- build_all.sh
+++ build_all.sh
@@ -2,6 +2,14 @@
 
 repo_root="$1"
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [ ! -d "${script_dir}/common" ]; then
+  >&2 echo "Error: 'common' directory not found."
+  >&2 echo "Remember to checkout submodules:"
+  >&2 echo "  git submodule update --init --recursive"
+  exit 1
+fi
+
 pushd $(dirname "$0")
 
 rm -rf build dist
