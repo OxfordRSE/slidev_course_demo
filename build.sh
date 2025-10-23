f6cb6cbca7cccfd76cdb35956ca11b58459d96bc
jsbrittain <98161205+jsbrittain@users.noreply.github.com>
2025-10-23 16:13:01 +0100

    Test

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ if [ -z "$dir" ]; then
     exit 1
 fi
 
+repo_root="$2"
+
 presentation_name=$(basename "$dir")
 echo "Presentation name: $presentation_name"
 
@@ -16,7 +18,7 @@ mkdir -p build/${presentation_name}
 cp -r common/* build/${presentation_name}/
 cp -r presentations/${presentation_name}/* build/${presentation_name}/
 
-npx slidev build --out dist --base /${{github.event.repository.name}}/${presentation_name}/ build/${presentation_name}/slides.md
+npx slidev build --out dist --base /${repo_root}/${presentation_name}/ build/${presentation_name}/slides.md
 
 # Remove all except 'dist' folder
 mv build/${presentation_name}/dist out/${presentation_name}
--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+repo_root="$2"
+
 pushd $(dirname "$0")
 
 rm -rf build out
@@ -8,7 +10,7 @@ mkdir -p build out
 echo "<html><h1>Presentations</h1>" > out/index.html
 
 for dir in presentations/*/; do
-    ./build.sh "$dir"
+    ./build.sh "$dir" "$repo_root"
     presentation_name=$(basename "$dir")
     echo "<li><a href=\"${presentation_name%/}/index.html\">${presentation_name%/}</a></li>" >> out/index.html
 done
